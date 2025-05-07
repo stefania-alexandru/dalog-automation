2d2a41358249c4017d365995bc1197ba805c39f3
stefania-alexandru <stefania.alexandru@digitalya.co>
2025-05-07 14:57:13 +0300

    authentification setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,27 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
+  },
+
   eslintConfigPrettier,
 ]);
